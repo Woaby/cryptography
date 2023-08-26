2f38bbd393aa0ba28fd0a4c2fab7fe30e93f7dc2
Woaby <thomasst@telfort.nl>
2023-08-27 00:31:04 +0200

    documentation

--- data/utils.py
+++ data/utils.py
@@ -1,22 +1,32 @@
-import random
+import random, json
 
-def getperm(l, key=6135584619):
-    # Calculate a seed based on the sum of ASCII values of characters.
+def getkey(key_name):
+    with open(key_name, "r") as json_file:
+        return json.load(json_file)
+
+def readeblecontent(input_name):
+    with open(input_name, "r") as txt:
+        return txt.read()
+
+def getperm(l, key):
+    randomseed = int(key["seed"])
+
+    # Calculate a seed based on the sum of ASCII values of characters + unique seed from key.
     seed = sum(ord(char) for char in l)
-    random.seed(seed + key)
+    random.seed(seed + randomseed)
     
     permutation = list(range(len(l)))
     random.shuffle(permutation)
     return permutation
 
-def shuffle(l):
-    permutation = getperm(l)
-    shuffled_list = [l[j] for j in permutation]
+def shuffle(l, key):
+    perm = getperm(l, key)
+    shuffled_list = [l[j] for j in perm]
 
     l[:] = shuffled_list
 
-def unshuffle(l, permutation):
+def unshuffle(l, perm):
     unshuffled_list = [None] * len(l)
-    for i, j in enumerate(permutation):
+    for i, j in enumerate(perm):
         unshuffled_list[j] = l[i]
     l[:] = unshuffled_list
